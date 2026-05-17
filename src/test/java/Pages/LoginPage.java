9ff0e82eb53989380d127c1c955107182c449bde
NdabeMazii <ndabezinhlemazibuko6@gmail.com>
2026-05-17 23:20:04 +0200

    Created LoginPage

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,35 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+        @FindBy(xpath = "//h2[@id='login-heading']")
+        WebElement loginHeading_xpath;
+
+        @FindBy(id = "login-email")
+        WebElement loginEmail_id;
+
+        @FindBy(id = "login-password")
+        WebElement loginPassword_id;
+
+        @FindBy(id = "login-submit")
+        WebElement loginSubmitButton_id;
+
+
+        public LoginPage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public void clickLoginButton() {
+            new WebDriverWait(driver, java.time.Duration.ofSeconds(15)).until(visibilityOf(loginHeading_xpath));
+            loginHeading_xpath.click();
+        }
+}
